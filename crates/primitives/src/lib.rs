6e12eafb65e14418939fe1176a1d9ade6f49c6e7
chirag-bgh <76247491+chirag-bgh@users.noreply.github.com>
2024-08-02 13:11:41 +0530

    chore: remove unused types (#26)

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -11,8 +11,6 @@ pub use cancel_preconf::{CancelPreconfRequest, CancelPreconfResponse};
 pub use check_preconf_response::{PreconfStatus, PreconfStatusResponse};
 pub use preconf_hash::PreconfHash;
 #[allow(unused_imports)]
-pub use preconf_request::{
-    InclusionMetaData, OrderingMetaData, PreconfCondition, PreconfRequest, TipTransaction,
-};
+pub use preconf_request::{OrderingMetaData, PreconfCondition, PreconfRequest, TipTransaction};
 pub use preconf_response::PreconfResponse;
 pub use proposer_info::ProposerInfo;
