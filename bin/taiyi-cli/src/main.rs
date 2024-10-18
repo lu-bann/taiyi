a23e5c703677729e132c79fe30ca0c71fa65ba50
WillQ <zsluedem06@gmail.com>
2024-10-18 09:24:56 +0800

    feat: add deposit command

---
+++ bin/taiyi-cli/src/main.rs
@@ -0,0 +1,29 @@
+use clap::{Parser, Subcommand};
+use taiyi_cli::{initialize_tracing_log, DepositCommand};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about = "taiyi-cli", long_about = None)]
+pub struct Cli {
+    /// The command to execute
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+/// Commands to be executed
+#[derive(Debug, Subcommand)]
+pub enum Commands {
+    #[command(name = "user-deposit")]
+    Deposit(DepositCommand),
+}
+
+fn main() -> eyre::Result<()> {
+    let cli = Cli::parse();
+    initialize_tracing_log();
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .expect("tokio runtime build failed");
+    match cli.command {
+        Commands::Deposit(cmd) => runtime.block_on(async { cmd.execute().await }),
+    }
+}
---
+++ crates/cli/src/utils.rs
@@ -0,0 +1,20 @@
+use std::str::FromStr;
+
+use tracing::Level;
+
+pub fn initialize_tracing_log() {
+    let level_env = std::env::var("RUST_LOG").unwrap_or("info".to_owned());
+    let level = if let Ok(level) = Level::from_str(&level_env) {
+        level
+    } else {
+        eprint!("Invalid log level {level_env}, defaulting to info");
+        Level::INFO
+    };
+
+    tracing_subscriber::fmt()
+        .compact()
+        .with_max_level(level)
+        .with_target(true)
+        .with_file(true)
+        .init();
+}
