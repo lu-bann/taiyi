9e622c74508ca52ad4b18875a75e1ae174e7fdb5
WillQ <zsluedem06@gmail.com>
2024-10-18 09:12:04 +0800

    fix: submit preconf request example

--- examples/src/submit-preconf-request.rs
+++ examples/src/submit-preconf-request.rs
@@ -2,23 +2,30 @@
 #![allow(unused_variables)]
 
 use alloy_consensus::TxEip1559;
-use alloy_primitives::{TxKind, U256};
+use alloy_primitives::{Address, Bytes, TxKind, U256};
 use alloy_provider::{Provider, ProviderBuilder};
 use alloy_signer::{Signature, SignerSync};
 use alloy_signer_local::PrivateKeySigner;
+use alloy_sol_types::sol;
 use reth_primitives::{Transaction, TransactionSigned, TransactionSignedEcRecovered};
 use taiyi_primitives::{AvailableSlotResponse, PreconfRequest, PreconfTx, TipTransaction};
+
+const TAIYI_CORE_ADDRESS: &str = "0xBc158E71537d843616D1fE0cc5e39900bB38cBff"; // taiyi core contract address for Helder
+const TAIYI_PRECONFER_ADDRESS: &str = "0xbC37A63E213a791c944a7EA104e67FB5B4b3DF07"; // taiyi preconfer contract address for Helder
+const CHAIN_ID: u64 = 7014190335; // currently only helder is supported
 
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let taiyi_url =
-        std::env::var("TAIYI_URL").unwrap_or_else(|_| "http://127.0.0.1:18550".to_string());
+        std::env::var("TAIYI_URL").unwrap_or_else(|_| "http://127.0.0.1:5656".to_string());
     let el_url = std::env::var("EL_URL").unwrap_or_else(|_| "http://127.0.0.1:8545".to_string());
     let signer_private = std::env::var("PRIVATE_KEY").expect("Input private key");
     let signer: PrivateKeySigner = signer_private.parse().unwrap();
     let provider = ProviderBuilder::new().with_recommended_fillers().on_builtin(&el_url).await?;
 
+    let sender = signer.address();
     let client = reqwest::Client::new();
+    let preconfer_address: Address = TAIYI_PRECONFER_ADDRESS.parse().unwrap();
     let res = client.get(&format!("{}/commitments/v1/slots", taiyi_url)).send().await?;
     let res_b = res.bytes().await?;
     println!("res: {:?}", res_b);
@@ -27,50 +34,60 @@ async fn main() -> eyre::Result<()> {
         println!("No available slot");
         return Ok(());
     }
-    let target_slot = available_slot.available_slots.last().expect("last").slot;
-    println!("target_slot: {:?}", available_slot);
+    println!("available_slot: {:?}", available_slot);
+    let target_slot = available_slot.available_slots.last().unwrap().slot;
+    println!("target_slot: {:?}", target_slot);
+    sol!(
+        #[sol(rpc)]
+        contract TaiyiCore{
+            function getTipNonce(address sender) public view returns (uint256 nonce);
+            function getPreconfNonce(address sender) public view returns (uint256 nonce);
+        }
+    );
 
-    // FIXME: get nonce from contract
-    let nonce = provider.get_transaction_count(signer.address()).await?;
+    let contract =
+        TaiyiCore::TaiyiCoreInstance::new(TAIYI_CORE_ADDRESS.parse().unwrap(), provider.clone());
+    let tip_nonce = contract.getTipNonce(sender).call().await?.nonce;
+    let preconf_nonce = contract.getPreconfNonce(sender).call().await?.nonce;
+    let estimate = provider.estimate_eip1559_fees(None).await?;
+    let to_addr: Address = "0x39e7971cF48AAf3222BDe5871d45829e274CD356".parse().unwrap();
+    let tip_tx = TipTransaction {
+        gas_limit: U256::from(21_000),
+        from: sender,
+        to: preconfer_address,
+        pre_pay: U256::from(estimate.max_fee_per_gas * 21_000),
+        after_pay: U256::from(estimate.max_fee_per_gas * 21_000),
+        nonce: tip_nonce,
+        target_slot: U256::from(target_slot),
+    };
+    let tip_hash = tip_tx.tip_tx_hash(U256::from(CHAIN_ID));
+    let tip_sig = signer.sign_hash_sync(&tip_hash)?;
+    let mut preconf_tx = PreconfTx {
+        from: sender,
+        to: to_addr,
+        value: U256::from(10000000000000000u64),
+        call_data: Default::default(),
+        call_gas_limit: U256::from(1000000),
+        nonce: preconf_nonce,
+        signature: Default::default(),
+    };
+    let preconf_hash = preconf_tx.hash();
+    let preconf_sig: Vec<u8> = signer.sign_hash_sync(&preconf_hash)?.into();
+    preconf_tx.signature = Bytes::from(preconf_sig);
+    let preconf_request = PreconfRequest {
+        tip_tx: tip_tx.into(),
+        tip_tx_signature: tip_sig,
+        preconf_tx: Some(preconf_tx),
+        preconfer_signature: None,
+        preconf_req_signature: None,
+    };
 
-    // let tx: Transaction = Transaction::Eip1559(TxEip1559 {
-    //     chain_id: 7014190335,
-    //     nonce,
-    //     max_priority_fee_per_gas: estimate.max_priority_fee_per_gas,
-    //     max_fee_per_gas: estimate.max_fee_per_gas,
-    //     gas_limit: 220000,
-    //     to: TxKind::Call("0xc998d0300e83d2Bf0eD9abB2A62D25A368adb8ED".parse().unwrap()),
-    //     value: U256::from(10),
-    //     input: Default::default(),
-    //     access_list: Default::default(),
-    // });
-    // let sig = signer.sign_hash_sync(&tx.signature_hash())?;
-    // let tx = TransactionSignedEcRecovered::from_signed_transaction(
-    //     TransactionSigned::from_transaction_and_signature(tx, sig),
-    //     signer.address(),
-    // );
-
-    // let tx_b = serde_json::to_vec(&tx.into_signed()).unwrap();
-    // let preconf_request = PreconfRequest {
-    //     preconf_tx: Some(tx_b),
-    //     tip_tx: TipTransaction::new(
-    //         Default::default(),
-    //         signer.address(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         Default::default(),
-    //         U256::from(target_slot),
-    //     ),
-    //     tip_tx_signature: Signature::,
-    //     preconfer_signature: Default::default(),
-    // };
-    // let res = client
-    //     .post(&format!("{}/commitments/v1/preconf_request", taiyi_url))
-    //     .json(&preconf_request)
-    //     .send()
-    //     .await?;
-    // let res_body = res.text().await?;
-    // println!("res: {}", res_body);
+    let res = client
+        .post(&format!("{}/commitments/v1/preconf_request", taiyi_url))
+        .json(&preconf_request)
+        .send()
+        .await?;
+    let res_body = res.text().await?;
+    println!("res: {}", res_body);
     Ok(())
 }
