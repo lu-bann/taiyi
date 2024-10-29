8d967ca6fc52af8ff7fbeaead65ccb2ac9a913d9
chirag-bgh <chirag2bsingh@gmail.com>
2024-10-29 17:34:56 +0530

    feat: fetch sender info from db

--- crates/preconfer/src/storage_slots.rs
+++ crates/preconfer/src/storage_slots.rs
@@ -9,4 +9,12 @@ lazy_static! {
     /// mapping(address => uint256) public lockBlock
     pub static ref LOCK_BLOCK_STORAGE_SLOT: StorageKey =
         StorageKey::from_hex("0x0000000000000000000000000000000000000000000000000000000000000002").expect("");
+
+    /// mapping(address => uint256) private tipNonceSequenceNumber
+    pub static ref TIP_NONCE_STORAGE_SLOT: StorageKey =
+        StorageKey::from_hex("0x0000000000000000000000000000000000000000000000000000000000000003").expect("");
+
+    /// mapping(address => uint256) private preconfNonceSequenceNumber
+    pub static ref PRECONF_NONCE_STORAGE_SLOT: StorageKey =
+        StorageKey::from_hex("0x0000000000000000000000000000000000000000000000000000000000000004").expect("");
 }
