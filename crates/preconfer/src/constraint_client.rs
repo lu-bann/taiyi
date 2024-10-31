5cea69e1859a2343baba2a00ebd616fd1da1ab46
Bob Liu <akagi201@gmail.com>
2024-10-31 11:01:44 +0800

    feat: use relative slot time

--- crates/preconfer/src/constraint_client.rs
+++ crates/preconfer/src/constraint_client.rs
@@ -1,22 +1,24 @@
 use std::time::SystemTime;
 
+use ethereum_consensus::deneb::Context;
 use reqwest::Url;
 use taiyi_primitives::SignedConstraintsMessage;
 
 use crate::metrics::preconfer::{PRECONF_CONSTRAINTS_SENT_TIME, RELAY_STATUS_CODE};
 
 /// Client used by commit modules to request signatures via the Signer API
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct ConstraintClient {
+    context: Context,
     url: Url,
     client: reqwest::Client,
 }
 
 impl ConstraintClient {
-    pub fn new(relay_server_address: String) -> eyre::Result<Self> {
+    pub fn new(context: Context, relay_server_address: String) -> eyre::Result<Self> {
         let client = reqwest::Client::builder().build()?;
 
-        Ok(Self { url: Url::parse(&relay_server_address)?, client })
+        Ok(Self { context, url: Url::parse(&relay_server_address)?, client })
     }
 
     pub async fn send_set_constraints(
@@ -28,13 +30,21 @@ impl ConstraintClient {
         let response = self.client.post(url.clone()).json(&constraint).send().await?;
         let code = response.status();
         RELAY_STATUS_CODE.with_label_values(&[code.as_str(), url.as_str()]).inc();
+        let genesis_time = match self.context.genesis_time() {
+            Ok(genesis_time) => genesis_time,
+            Err(_) => self.context.min_genesis_time + self.context.genesis_delay,
+        };
+        let slot_start_timestamp =
+            genesis_time + (constraint.message.slot * self.context.seconds_per_slot);
+        let slot_start_timestamp_ms = slot_start_timestamp * 1000;
         let now = SystemTime::now();
+        let slot_diff_time =
+            now.duration_since(SystemTime::UNIX_EPOCH).expect("get system error").as_millis()
+                as f64
+                - slot_start_timestamp_ms as f64;
         PRECONF_CONSTRAINTS_SENT_TIME
             .with_label_values(&[constraint.message.slot.to_string().as_str()])
-            .observe(
-                now.duration_since(SystemTime::UNIX_EPOCH).expect("get system error").as_millis()
-                    as f64,
-            );
+            .observe(slot_diff_time);
 
         if code.is_success() {
             Ok(())
