c86465c65f6241ef24f62e34d0a64243d9d19e72
chirag-bgh <chirag2bsingh@gmail.com>
2024-11-10 21:15:30 +0530

    more cleanup

--- crates/preconfer/src/preconf_api/mod.rs
+++ crates/preconfer/src/preconf_api/mod.rs
@@ -6,12 +6,14 @@ use alloy_signer_local::PrivateKeySigner;
 use api::PreconfApiServer;
 use blst::min_pk::SecretKey;
 use ethereum_consensus::deneb::Context;
+use reth_revm::handler::execution;
 use state::PreconfState;
 use tracing::{error, info};
 
 use crate::{
     constraint_client::ConstraintClient,
     lookahead_fetcher::run_cl_process,
+    metrics::provider,
     network_state::NetworkState,
     preconfer::Preconfer,
     pricer::{ExecutionClientFeePricer, TaiyiFeePricer},
@@ -32,28 +34,18 @@ pub async fn spawn_service(
     ecdsa_signer: PrivateKeySigner,
     relay_url: Vec<String>,
 ) -> eyre::Result<()> {
-    let provider =
-        ProviderBuilder::new().with_recommended_fillers().on_builtin(&execution_client_url).await?;
-    let chain_id = provider.get_chain_id().await?;
-    let network_state = NetworkState::new(0, Vec::new());
     let constraint_client =
         ConstraintClient::new(relay_url.first().expect("relay_url is empty").clone())?;
 
-    // let bls_pk = bls_private_key.sk_to_pk();
-
-    info!("preconfer is on chain_id: {:?}", chain_id);
-
-    let base_fee_fetcher = ExecutionClientFeePricer::new(provider.clone());
-
-    let validator = Preconfer::new(provider.clone(), taiyi_core_contract_addr, base_fee_fetcher);
+    let provider =
+        ProviderBuilder::new().with_recommended_fillers().on_builtin(&execution_client_url).await?;
 
     let state = PreconfState::new(
-        validator,
-        network_state,
         constraint_client,
         context,
         bls_private_key,
         ecdsa_signer,
+        taiyi_core_contract_addr,
         provider.clone(),
     )
     .await;
