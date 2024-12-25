3b55f27a70d224d47b7170b3e6ca9f7e825083b7
chirag-bgh <chirag2bsingh@gmail.com>
2024-12-25 17:55:02 +0530

    fix: initialise script

--- scripts/devnet/initialize.sh
+++ scripts/devnet/initialize.sh
@@ -14,6 +14,7 @@ popd
 # TAIYI CORE would be 0xA791D59427B2b7063050187769AC871B497F4b3C
 if [ "$(cast code 0x0A79920c296E86e7BB12Ad20ca7Ffbbd7AE5905B --rpc-url $EXECUTION_URL)" == "0x" ]; then
     pushd contracts
+    git submodule update --progress --init
     PRIVATE_KEY="c5114526e042343c6d1899cad05e1c00ba588314de9b96929914ee0df18d46b2" \
     bash script/deploy.sh
     popd 
