bb51dcd480b6039769792f347f6156d90dd8d1ea
WillQ <zsluedem06@gmail.com>
2025-01-11 14:58:44 +0800

    fix: fix ci code size too large

--- contracts/script/deploy.sh
+++ contracts/script/deploy.sh
@@ -1,6 +1,7 @@
 if [ -z "$EXECUTION_URL" ]; then
     export EXECUTION_URL="http://localhost:8545"
 fi
+export FOUNDRY_PROFILE=prod
 forge script --rpc-url $EXECUTION_URL \
 -vvvv --private-key $PRIVATE_KEY --broadcast ./script/Deployments.s.sol:Deploy \
 --sig "run(string memory configFile)" \
