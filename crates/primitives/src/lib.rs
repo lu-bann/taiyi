bfaf315b5f964296532af8668786b8d05c25730c
chirag-bgh <chirag2bsingh@gmail.com>
2025-03-03 16:49:33 +0530

    chore: cleanup

--- crates/primitives/src/lib.rs
+++ crates/primitives/src/lib.rs
@@ -1,11 +1,9 @@
 mod constraints;
 mod preconf_fee;
-mod preconf_request;
+mod preconf_request_type_b;
 mod preconf_response;
-mod preconf_tx_request;
 
 pub use constraints::{ConstraintsMessage, SignableBLS, SignedConstraints};
 pub use preconf_fee::PreconfFeeResponse;
-pub use preconf_request::{BlockspaceAllocation, PreconfRequest, SubmitTransactionRequest};
+pub use preconf_request_type_b::{BlockspaceAllocation, PreconfRequest, SubmitTransactionRequest};
 pub use preconf_response::PreconfResponse;
-pub use preconf_tx_request::PreconfTxRequest;
