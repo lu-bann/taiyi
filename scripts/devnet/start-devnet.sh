7b3f000a8dcdab5f33854b79be192efe672e1910
WillQ <zsluedem06@gmail.com>
2025-04-10 12:27:09 +0800

    chore: ci use ghcr

--- scripts/devnet/start-devnet.sh
+++ scripts/devnet/start-devnet.sh
@@ -2,6 +2,11 @@ set -xe
 # set default enclave name
 source "$(dirname "$0")/config.sh"
 
+if [ -z "$TAIYI_BOOST_IMAGE" ]; then
+  export TAIYI_BOOST_IMAGE="lubann/taiyi:latest"
+fi
+
+sed -i "s|lubann/taiyi:latest|${TAIYI_BOOST_IMAGE}|g" scripts/devnet/luban.yml
 
 pushd $WORKING_DIR
 kurtosis run --enclave $ENCLAVE_NAME github.com/lu-bann/ethereum-package@taiyi --args-file scripts/devnet/luban.yml
