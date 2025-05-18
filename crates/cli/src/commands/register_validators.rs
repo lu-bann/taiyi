1e32bc81318022dbaeb320d8d2fb85a0917262b0
WillQ <zsluedem06@gmail.com>
2025-05-18 22:39:35 +0800

    update register command

--- crates/cli/src/commands/register_validators.rs
+++ crates/cli/src/commands/register_validators.rs
@@ -7,10 +7,16 @@ use alloy_signer_local::PrivateKeySigner;
 use alloy_sol_types::SolValue;
 use clap::Parser;
 use eyre::Result;
+use lighthouse_eth2_keystore::Keystore;
 use reqwest::Url;
 use taiyi_contracts::{SignedRegistration, TaiyiMiddleware};
 use taiyi_crypto::{sign, to_public_key};
-use tracing::info;
+use tracing::{debug, info};
+
+use crate::{
+    keys_management::keystore::{keystore_paths, KeystoreError, KeystoreSecret},
+    keysource::KeySource,
+};
 
 const DOMAIN_SEPARATOR: [u8; 4] = hex_literal::hex!("00555243"); // "URC" in little endian
 
@@ -28,10 +34,6 @@ pub struct RegisterValidatorsCommand {
     #[clap(long, env = "TAIYI_MIDDLEWARE_ADDRESS")]
     taiyi_middleware_address: Address,
 
-    /// Private key for signing transactions
-    #[clap(long, env = "BLS_PRIVATE_KEY", value_delimiter = ',')]
-    bls_private_keys: Vec<String>,
-
     /// Operator address
     #[clap(long, env = "OPERATOR_ADDRESS")]
     operator_address: Address,
@@ -39,6 +41,10 @@ pub struct RegisterValidatorsCommand {
     /// Value of the transaction
     #[clap(long, env = "COLLATERAL")]
     collateral: U256,
+
+    /// The source of the private key.
+    #[clap(subcommand)]
+    source: KeySource,
 }
 
 impl RegisterValidatorsCommand {
@@ -50,20 +56,32 @@ impl RegisterValidatorsCommand {
             .wallet(wallet.clone())
             .on_http(Url::from_str(&self.execution_rpc_url)?);
 
-        let bls_signatures = self
-            .bls_private_keys
-            .clone()
-            .into_iter()
-            .map(|k| {
-                let key_bytes = hex::decode(k)?;
-                let sk = U256::from_be_slice(&key_bytes);
-                let pubkey = to_public_key(sk)?;
-                let message = self.operator_address.abi_encode();
+        let bls_signatures = match &self.source {
+            KeySource::SecretKeys { secret_keys } => secret_keys
+                .clone()
+                .into_iter()
+                .map(|k| {
+                    let key_bytes = hex::decode(k)?;
+                    let sk = U256::from_be_slice(&key_bytes);
+                    let pubkey = to_public_key(sk)?;
+                    let message = self.operator_address.abi_encode();
 
-                let sig = sign(sk, &message, &DOMAIN_SEPARATOR)?;
-                Ok(SignedRegistration { pubkey, signature: sig })
-            })
-            .collect::<Result<Vec<_>>>()?;
+                    let sig = sign(sk, &message, &DOMAIN_SEPARATOR)?;
+                    Ok(SignedRegistration { pubkey, signature: sig })
+                })
+                .collect::<Result<Vec<_>>>()?,
+            KeySource::LocalKeystore { opts } => {
+                let keystore_secret = KeystoreSecret::from_keystore_options(opts)?;
+                generate_signed_registrations_from_keystore(
+                    &opts.path,
+                    keystore_secret,
+                    self.operator_address,
+                )?
+            }
+            KeySource::Dirk { .. } => {
+                unimplemented!("Dirk is not supported yet");
+            }
+        };
 
         // Create middleware contract interface
         let middleware = TaiyiMiddleware::new(self.taiyi_middleware_address, provider.clone());
@@ -79,4 +97,26 @@ impl RegisterValidatorsCommand {
         info!("Transaction receipt got. Confirmed in the block {:?}", receipt.block_number);
         Ok(())
     }
+}
+pub fn generate_signed_registrations_from_keystore(
+    keys_path: &str,
+    keystore_secret: KeystoreSecret,
+    operator_address: Address,
+) -> Result<Vec<SignedRegistration>> {
+    let keystores_paths = keystore_paths(keys_path)?;
+    let mut signed_messages = Vec::with_capacity(keystores_paths.len());
+    debug!("Found {} keys in the keystore", keystores_paths.len());
+
+    for path in keystores_paths {
+        let ks = Keystore::from_json_file(path).map_err(KeystoreError::Eth2Keystore)?;
+        let password = keystore_secret.get(ks.pubkey()).ok_or(KeystoreError::MissingPassword)?;
+        let kp = ks.decrypt_keypair(password.as_bytes()).map_err(KeystoreError::Eth2Keystore)?;
+        let sk = U256::from_be_slice(kp.sk.serialize().as_bytes());
+        let validator_pubkey = to_public_key(sk)?;
+        let message = operator_address.abi_encode();
+        let sig = sign(sk, &message, &DOMAIN_SEPARATOR)?;
+        let signed = SignedRegistration { pubkey: validator_pubkey, signature: sig };
+        signed_messages.push(signed);
+    }
+    Ok(signed_messages)
 }
