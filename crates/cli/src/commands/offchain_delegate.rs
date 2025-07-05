dc1182f3345658927d7f252bc68897683d8e60ed
WillQ <zsluedem06@gmail.com>
2025-07-05 21:57:16 +0800

    fix: delegation digest with compress pubkey

--- crates/cli/src/commands/offchain_delegate.rs
+++ crates/cli/src/commands/offchain_delegate.rs
@@ -178,9 +178,8 @@ impl DelegationMessage {
     pub fn digest(&self) -> [u8; 32] {
         let mut hasher = Sha256::new();
         hasher.update([self.action]);
-        hasher.update(self.validator_pubkey.serialize());
-        hasher.update(self.delegatee_pubkey.serialize());
-
+        hasher.update(self.validator_pubkey.compress());
+        hasher.update(self.delegatee_pubkey.compress());
         hasher.finalize().into()
     }
 }
@@ -211,9 +210,8 @@ impl RevocationMessage {
     pub fn digest(&self) -> [u8; 32] {
         let mut hasher = Sha256::new();
         hasher.update([self.action]);
-        hasher.update(self.validator_pubkey.serialize());
-        hasher.update(self.underwriter_pubkey.serialize());
-
+        hasher.update(self.validator_pubkey.compress());
+        hasher.update(self.underwriter_pubkey.compress());
         hasher.finalize().into()
     }
 }
