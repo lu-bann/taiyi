6b564e2801907a5e88eda1afc8156e056bf35911
Umberto <umberto.valleriani@gmail.com>
2025-07-22 16:14:07 +0200

    remove linglong repo folder in clean.sh (make e2e-clean)

--- scripts/devnet/clean.sh
+++ scripts/devnet/clean.sh
@@ -2,6 +2,8 @@ set -xe
 
 source "$(dirname "$0")/config.sh"
 
+# `|| true` ignore error to continue even if we did `set -e`
+rm -rf linglong || true
 kurtosis enclave rm -f $ENCLAVE_NAME
 pushd $WORKING_DIR
 rm -rf 1-lighthouse-geth-0-63
